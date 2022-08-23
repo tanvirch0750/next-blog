ce63f2df86414ac2385f08231c387da974313b6e
tanvirch0750 <tanvir0750@yahoo.com>
2022-08-24 01:08:38 +0600

    completed hero component

--- components/home-page/Hero.js
+++ components/home-page/Hero.js
@@ -1,5 +1,24 @@
+import Image from 'next/image';
+import classes from './Hero.module.css';
+
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/tanvir.jpg"
+          width="300"
+          height="300"
+          alt="A image of Tanvir"
+        />
+      </div>
+      <h1>Hi, I am Tanvir Chowdhury</h1>
+      <p>
+        I blog about frontend development - Especially frontend development
+        related technologies like Javascript, React, Html, Css, Tailwind etc.
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
