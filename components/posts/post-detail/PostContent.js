4a25eacf6444ef798137fbc7e7164ccf2874a557
tanvirch0750 <tanvir0750@yahoo.com>
2022-08-27 01:44:48 +0600

    Rendering Dynamic post pages and paths

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,21 +2,12 @@ import ReactMarkdown from 'react-markdown';
 import classes from './PostContent.module.css';
 import PostHeader from './PostHeader';
 
-const DUMMY_POST = {
-  title: 'Getting Started With Next Js',
-  image: 'getting-started-with-next-js.png',
-  date: '2022-02-10',
-  slug: 'getting-started-with-next-js',
-  content: '# This is first content',
-};
-
-const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
-
-const PostContent = () => {
+const PostContent = ({ post }) => {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,11 +1,37 @@
 import PostContent from '../../components/posts/post-detail/PostContent';
+import { getPostData, getPostsFiles } from '../../lib/post-utils';
 
-const SinglePostPage = () => {
+const SinglePostPage = ({ post }) => {
   return (
     <>
-      <PostContent />
+      <PostContent post={post} />
     </>
   );
 };
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
 
 export default SinglePostPage;
