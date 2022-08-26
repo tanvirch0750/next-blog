e4abe4e979b9c47379e637dfb16a68d59b6f4ef4
tanvirch0750 <tanvir0750@yahoo.com>
2022-08-27 02:21:49 +0600

    rendering images with next image

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,13 +1,39 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import classes from './PostContent.module.css';
 import PostHeader from './PostHeader';
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+      <img src="/" alt="" />
     </article>
   );
 };
