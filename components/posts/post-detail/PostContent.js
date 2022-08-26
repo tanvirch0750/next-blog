e93857bbf02e85b661426d0c1bcd3b61a0c02bc0
tanvirch0750 <tanvir0750@yahoo.com>
2022-08-27 02:45:30 +0600

    Rendering code snippets from markdown

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import classes from './PostContent.module.css';
 import PostHeader from './PostHeader';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const PostContent = ({ post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -13,10 +15,12 @@ const PostContent = ({ post }) => {
       if (node.children[0].tagName === 'img') {
         const image = node.children[0];
 
+        console.log(image);
+
         return (
           <div className={classes.image}>
             <Image
-              src={`/images/posts/${post.slug}/${image.url}`}
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
               alt={image.alt}
               width={600}
               height={300}
@@ -27,13 +31,25 @@ const PostContent = ({ post }) => {
 
       return <p>{paragraph.children}</p>;
     },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          // eslint-disable-next-line react/no-children-prop
+          children={children}
+        />
+      );
+    },
   };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
-      <img src="/" alt="" />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
