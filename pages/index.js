83f5fd7314133bb2ed36f479139547f26b668029
tanvirch0750 <tanvir0750@yahoo.com>
2022-08-27 00:50:23 +0600

    used markdown data for Rendering posts using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,24 @@
 import FeaturedPosts from '../components/home-page/FeaturedPosts';
 import Hero from '../components/home-page/Hero';
+import { getFeaturedPosts } from '../lib/post-utils';
 
-const DUMMY_DATA = [
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js2',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js3',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js4',
-  },
-];
-
-const HomePage = () => {
+const HomePage = ({ posts }) => {
   return (
     <>
       <Hero />
-      <FeaturedPosts posts={DUMMY_DATA} />
+      <FeaturedPosts posts={posts} />
     </>
   );
 };
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
+}
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,46 +1,22 @@
 import AllPosts from '../../components/posts/AllPosts';
+import { getAllPosts, getFeaturedPosts } from '../../lib/post-utils';
 
-const DUMMY_DATA = [
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js2',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js3',
-  },
-  {
-    title: 'Getting Started With Next Js',
-    image: 'getting-started-with-next-js.png',
-    excerpt:
-      'Next Js is React Framework for Production. It makes building fullstack React Application easier',
-    date: '2022-02-10',
-    slug: 'getting-started-with-next-js4',
-  },
-];
-
-const AllPostsPage = () => {
+const AllPostsPage = ({ posts }) => {
   return (
     <div>
-      <AllPosts posts={DUMMY_DATA} />
+      <AllPosts posts={posts} />
     </div>
   );
 };
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostsPage;
